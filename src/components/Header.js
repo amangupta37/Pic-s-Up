eb44994dd61e9b63a936b82c8b88a9b6f3d3044a
amangupta37 <amangupta6121998@gmail.com>
2021-04-18 09:11:19 +0530

    addeding upload feature

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,16 +3,18 @@ import PermMediaIcon from '@material-ui/icons/PermMedia';
 
 function Header() {
     return (
-        <div className="header">
+    <div  className="header" >
+
             <div className="app-name">
-                  <PermMediaIcon className="logo-icon" />  
-                  <h2>Pic's<span> - </span>Up</h2>  
+                <PermMediaIcon className="logo-icon" />  
+                <h2>Pic's<span> - </span>Up</h2>  
             </div>
             <div className="title">
                 <h3>The internet’s source of freely-usable images</h3> 
-                 <p># Contribute with quality contents</p>
+                <p># Contribute with quality contents</p>
             </div>
-        </div>
+            
+    </div>
     )
 }
 
--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -1,6 +1,11 @@
 import React from 'react'
 import useStorage from '../hooks/useStorage'
 function ProgressBar({ data , setData }) {
+
+    const {url , progress} =useStorage(data);
+
+    console.log(progress, url);
+
     return (
         <div className="progress-bar">
           <h1>progress is now </h1>
